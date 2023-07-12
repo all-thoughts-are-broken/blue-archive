3d2903e072e69413898a9e8a8feb7c5bcf95c298
万念断绝 <puzuoih9856@163.com>
2023-07-12 19:06:04 +0000

    add 学生列表（残次品）
    Signed-off-by: 万念断绝 <puzuoih9856@163.com>

---
+++ apps/学生列表.js
@@ -0,0 +1,23 @@
+import plugin from '../../../lib/plugins/plugin.js'
+
+export class example extends plugin {
+    constructor() {
+      super({
+        name: '学生列表',
+        dsc: 'emmm......',
+        event: 'message',
+        priority: 5000,
+        rule: [
+          {
+            reg: '^#学生列表$',
+            fnc: 'liebiao',
+          },
+        ],
+      });
+    }
+    async liebiao (e){   
+        let msg = [segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/学生列表.png`)]
+         await e.reply(msg)
+         return true
+    }
+}    
