189eceefbcd27ada20184b2149403ff552681623
万念断绝 <3410586885@qq.com>
2023-07-30 22:33:43 +0800

    氵个总力战

---
+++ apps/总力战.js
@@ -0,0 +1,54 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import common from '../../../lib/common/common.js'
+const _path=process.cwd()
+
+
+
+
+export class zonglizhan extends plugin {
+    constructor() {
+      super({
+        name: '总力战',
+        dsc: '攻略',
+        event: 'message',
+        priority: 3000,
+        rule: [{
+            reg: '^#?总力战帮助$',
+            fnc: 'help',
+               },
+               {
+            reg: '^#?格里高利$',
+            fnc: 'guregoreo',
+               }
+        ]
+      })
+    }
+
+   async help (e) {
+           let txt = ["目前共有九个总力战boss\n1.GOZ(戈兹)\n2.HOD(霍德)\n3.CHESED(眼球)\n4.BINAH(大蛇)\n5. KAITEN FX Mk.0(寿司)\n6.HIERONYMUS(主教)\n7.GREGORIUS(格里高利)\n8.SHIRO & KURO(黑与白)\n9.PEROROOZILLA(鸡斯拉)\n目前就只做了格里高利的"]
+
+           await e.reply(txt)
+           return true
+
+   }
+
+   async guregoreo (e) {
+         let msgs = [
+             "7个难度的属性图如下",
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/normal.png`),
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/hard.png`),
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/veryhard.png`),
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/hardcore.png`),
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/extreme.png`),
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/insane.png`),
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/torment.png`),
+             "格里高利的机制图如下",
+             segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/格里高利/机制.png`),
+             "以上就是全部攻略"
+                    ]
+
+        let forwardMsg = await common.makeForwardMsg(e, msgs, '格里高利')    //制作转发的消息
+        await e.reply(forwardMsg)
+
+   }
+}   
