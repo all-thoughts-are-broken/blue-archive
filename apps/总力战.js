312120411d846744e17a4780b592c58a1be9c810
万念断绝 <3410586885@qq.com>
2023-08-04 18:09:55 +0800

    添加大蛇总力战攻略

--- apps/总力战.js
+++ apps/总力战.js
@@ -19,13 +19,17 @@ export class zonglizhan extends plugin {
                {
             reg: '^#?格里高利$',
             fnc: 'guregoreo',
+               },
+               {
+            reg: '^#?大蛇$',
+            fnc: 'binah',
                }
         ]
       })
     }
 
    async help (e) {
-           let txt = ["目前共有九个总力战boss\n1.GOZ(戈兹)\n2.HOD(霍德)\n3.CHESED(眼球)\n4.BINAH(大蛇)\n5. KAITEN FX Mk.0(寿司)\n6.HIERONYMUS(主教)\n7.GREGORIUS(格里高利)\n8.SHIRO & KURO(黑与白)\n9.PEROROOZILLA(鸡斯拉)\n目前就只做了格里高利的"]
+           let txt = ["目前共有九个总力战boss\n1.GOZ(戈兹)\n2.HOD(霍德)\n3.CHESED(眼球)\n4.BINAH(大蛇)\n5. KAITEN FX Mk.0(寿司)\n6.HIERONYMUS(主教)\n7.GREGORIUS(格里高利)\n8.SHIRO & KURO(黑与白)\n9.PEROROOZILLA(鸡斯拉)\n目前就只做了格里高利和大蛇的"]
 
            await e.reply(txt)
            return true
@@ -51,4 +55,24 @@ export class zonglizhan extends plugin {
         await e.reply(forwardMsg)
 
    }
+
+   async binah (e) {
+    let msgs = [
+        "7个难度的属性图如下",
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/normal.png`),
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/hard.png`),
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/veryhard.png`),
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/hardcore.png`),
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/extreme.png`),
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/insane.png`),
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/torment.png`),
+        "格里高利的机制图如下",
+        segment.image(`file:///${_path}/plugins/BlueArchive-plugin/resources/总力战/大蛇/机制.jpg`),
+        "以上就是全部攻略"
+               ]
+
+   let forwardMsg = await common.makeForwardMsg(e, msgs, '大蛇')    //制作转发的消息
+   await e.reply(forwardMsg)
+
+}
 }   
