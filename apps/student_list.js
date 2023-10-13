c513d85ab61b8d7b57c6e0caf1fe5765dcb63493
万念断绝 <3410586885@qq.com>
2023-10-13 04:30:45 +0000

    更新学生列表
    Signed-off-by: 万念断绝 <3410586885@qq.com>

---
+++ apps/student_list.js
@@ -0,0 +1,46 @@
+import List from "../model/list.js";
+import puppeteer from "../../../lib/puppeteer/puppeteer.js";
+import md5 from "md5";
+
+export class list extends plugin {
+    constructor(e) {
+        super({
+            name: "学生列表",
+            dsc: "emmm",
+            event: "message",
+            priority: 500,
+            rule: [
+                {
+                    reg: "^/学生列表$",
+                    fnc: "list",
+                },
+            ],
+        });
+    }
+
+    async list() {
+        let data = await List.get(this.e);
+        if (!data) {
+            return;
+        }
+        let img = await this.cache(data);
+        await this.reply(img);
+    }
+
+    async cache(data) {
+        let tmp = md5(JSON.stringify(data));
+        if (list.studentData.md5 === tmp) {
+            return list.studentData.img;
+        }
+
+        list.studentData.img = await puppeteer.screenshot("list", data);
+        list.studentData.md5 = tmp;
+
+        return list.studentData.img;
+    }
+
+    static studentData = {
+        md5: "",
+        img: "",
+    };
+}
